b36b45069a683247cfe8ef81f5ecbff15e9ed44b
UdayBari3421 <115573575+UdayBari3421@users.noreply.github.com>
2023-09-18 09:42:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
-    //Write your code here
+	const fname = document.getElementsByName("fname")[0].value;
+	const lname = document.getElementsByName("lname")[0].value;
+	const full = fname + " "  + lname;
 
+	alert(full);
 }
